ad38e96a7521cb5be2427a5371ccfb1997ffd52c
Reuben Michael <52567822+reubenmichael@users.noreply.github.com>
2021-11-12 16:09:03 +1100

    feat(form.js): set up form defaults

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 // import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import ApiForm from './views/api-form'
 
-function App() {
-  return (
-    <ApiForm />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+// function App() {
+class App extends Component {
+  // state = {
+  //   postcode: Number(1234),
+  //   suburb: 'Surry Hills',
+  //   state: ''
+  // }
+
+  // handleSubmit = formContents => {
+  //   let postcode = formContents.postcode
+  //   let suburb = formContents.suburb
+  //   let state = formContents.state
+  //   this.setState({
+  //     postcode: postcode,
+  //     suburb: suburb,
+  //     state: state,
+  //   })
+  // }
+
+  render () {
+
+    // const { postcode, suburb, state } = this.state
+
+    // console.log('Here1: ' + postcode + suburb)
+  
+    return (
+  
+      <ApiForm />
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+
+  }
 }
 
 export default App;
--- src/views/api-form/form.js
+++ src/views/api-form/form.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.initialState = {
+            // postcode: this.props.postcode,
+            // suburb: this.props.suburb,
+            // state: this.props.state,
+            postcode: Number(2010),
+            suburb: 'Surry Hills',
+            state: '',
+        }
+
+        // console.log('Here2: ' + this.props.postcode + this.props.suburb)
+
+        this.state = this.initialState
+    }
+
+    // handleChange = event => {
+    //     const { name, value } = event.target
+    //     this.setState({
+    //         [name]: value
+    //     })
+    // }
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        console.log('Form submitted')
+        // this.props.handleSubmit(this.state)
+    }
+
+    // onFormSubmit = (event) => {
+    //     event.preventDefault();
+        
+    //     this.props.handleSubmit(this.state);
+    //     this.setState(this.initialState);
+    // }
 
     render () {
+        const { postcode, suburb } = this.state
+
+        console.log(postcode + suburb)
+        // onChange={this.handleChange}
+
         return (
-            <form>
-                <label for="postcode">Postcode</label>
-                <input type="number" name="postcode" id="field-1" minlength="4" maxlength="4" required placeholder="Your postcode" />
-                 <label for="suburb">Suburb</label>
-                <input type="text" name="suburb" id="field-2" minlength="1" required placeholder="Your suburb" />
-                <label for="state">State</label>
-                <select name="state" id="field-3" >
-                    <option disabled selected>Please select</option>
+            <form onSubmit={this.onFormSubmit}>
+                <label htmlFor="postcode">Postcode</label>
+                <input type="number" name="postcode" id="field-1" required placeholder="Your postcode" defaultValue={postcode}/>
+                <label htmlFor="suburb">Suburb</label>
+                <input type="text" name="suburb" id="field-2" required placeholder="Your suburb" defaultValue={suburb}/>
+                <label htmlFor="state">State</label>
+                <select name="state" id="field-3" defaultValue="NSW">
+                    {/* <option disabled >Please select</option> */}
                     <option value="ACT">ACT</option>
                     <option value="NSW">NSW</option>
                     <option value="NT">NT</option>
